08673c972fa6e64dafd17c60ae16f59d4722cebc
GregKalinin <gregkalini@yandex.ru>
2023-09-24 11:06:43 +0300

    update test

---
+++ conftest.py
@@ -0,0 +1,43 @@
+import os
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
+
+@pytest.fixture(scope='function', params=['chrome', 'firefox'])
+def open_browsers(request):
+    if request.param == 'chrome':
+        chromeOptions = webdriver.ChromeOptions()
+        service_chrome = Service(executable_path=ChromeDriverManager(version="114.0.5735.90").install())
+        chromeOptions.add_experimental_option("prefs", {"download.default_directory": f"{os.getcwd()}\downloads"})
+        chromeOptions.add_argument("--start-maximized")
+        # chromeOptions.add_argument("--headless=new") #-- Без отображения окна браузера
+        chrome_d = webdriver.Chrome(service=service_chrome, options=chromeOptions)
+        driver = chrome_d
+        request.cls.driver = driver
+        driver.get('https://demoqa.com/')
+        driver.implicitly_wait(10)
+        print('\nОткрыт браузер Chrome')
+        yield
+        driver.quit()
+
+
+    elif request.param == 'firefox':
+        firefoxOptions = webdriver.FirefoxProfile()
+        options = webdriver.FirefoxOptions()
+        service_firefox = Service(executable_path=GeckoDriverManager().install())
+        firefoxOptions.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/zip")
+        firefoxOptions.set_preference("browser.download.manager.showWhenStarting", False)
+        firefoxOptions.set_preference("browser.download.folderList", 2)
+        firefoxOptions.set_preference("browser.download.dir", f"{os.getcwd()}\downloads")
+        # options.add_argument("--headless") #-- Без отображения окна браузера
+        firefox_d = webdriver.Firefox(service=service_firefox, firefox_profile=firefoxOptions, options=options)
+        driver = firefox_d
+        request.cls.driver = driver
+        driver.get('https://demoqa.com/')
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        print('\nОткрыт браузер Firefox')
+        yield
+        driver.quit()
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,6 +6,7 @@ from page.methods import Methods
 from base.locators import Locators
 
 @allure.feature("Функционал регистрации студента")
+@pytest.mark.usefixtures('open_browsers')
 class Test_Student_Registration_Form:
 
     @allure.title('РЕГИСТРАЦИЯ СТУДЕНТА')
@@ -13,77 +14,69 @@ class Test_Student_Registration_Form:
     @severity(severity_level.CRITICAL)
     @pytest.mark.run(order=1)
     @pytest.mark.smoke
-    @pytest.mark.parametrize('params', ['chrome', 'firefox'])
-    def test_student_registration_form(self, params):
-        if params == 'chrome':
-            Methods.browser_open_chrome(self)
-        elif params == 'firefox':
-            Methods.browser_open_firefox(self)
+    def test_student_registration_form(self):
         create_page = Methods(self.driver)
 
-        try:
+        # Прокрутка страницы вниз
+        create_page.scrolling_page_down()
 
-            # Прокрутка страницы вниз
-            create_page.scrolling_page_down()
+        # Переход в модуль "Формы"
+        create_page.element_to_be_clickable(Locators.FORMS_MODULE)
 
-            # Переход в модуль "Формы"
-            create_page.element_to_be_clickable(Locators.FORMS_MODULE)
+        # Переход на страницу регистрации студента
+        create_page.element_to_be_clickable(Locators.STUDENT_REG_FORM)
 
-            # Переход на страницу регистрации студента
-            create_page.element_to_be_clickable(Locators.STUDENT_REG_FORM)
+        # Проверка названия страницы
+        create_page.text_on_pages(Locators.TITLE_NAME_STUDENT_REG_FORM, 'Student Registration Form')
 
-            # Проверка названия страницы
-            create_page.text_on_pages(Locators.TITLE_NAME_STUDENT_REG_FORM, 'Student Registration Form')
+        # Поле ввода "Имя"
+        create_page.field_optional_filling(Locators.FIRST_NAME_FIELD_STUDENT_REG_FORM, 'Имя:', BaseData.first_name_student)
 
-            # Поле ввода "Имя"
-            create_page.field_optional_filling(Locators.FIRST_NAME_FIELD_STUDENT_REG_FORM, 'Имя:', BaseData.first_name_student)
+        # Поле ввода "Фамилия"
+        create_page.field_optional_filling(Locators.LAST_NAME_FIELD_STUDENT_REG_FORM, 'Фамилия:', BaseData.last_name_student)
 
-            # Поле ввода "Фамилия"
-            create_page.field_optional_filling(Locators.LAST_NAME_FIELD_STUDENT_REG_FORM, 'Фамилия:', BaseData.last_name_student)
+        # Поле ввода "Email"
+        create_page.field_optional_filling(Locators.EMAIL_FIELD_STUDENT_REG_FORM, 'Email:', BaseData.email_student)
 
-            # Поле ввода "Email"
-            create_page.field_optional_filling(Locators.EMAIL_FIELD_STUDENT_REG_FORM, 'Email:', BaseData.email_student)
+        # Выбор пола
+        create_page.radio_button_click(Locators.MALE_RADIO_BUTTON_STUDENT_REG_FORM)
 
-            # Выбор пола
-            create_page.radio_button_click(Locators.MALE_RADIO_BUTTON_STUDENT_REG_FORM)
+        # Поле ввода "Мобильный номер"
+        create_page.field_optional_filling(Locators.MOBILE_NUMBER_FIELD_STUDENT_REG_FORM, 'Мобильный номер:',
+                                                    BaseData.mobile_number_student)
 
-            # Поле ввода "Мобильный номер"
-            create_page.field_optional_filling(Locators.MOBILE_NUMBER_FIELD_STUDENT_REG_FORM, 'Мобильный номер:',
-                                                        BaseData.mobile_number_student)
+        # Поле ввода "Дата рождения"
+        create_page.field_optional_filling(Locators.DATE_OF_BIRTH_FIELD_STUDENT_REG_FORM, 'Дата рождения:',
+                                                    BaseData.date_of_birth_student)
 
-            # Поле ввода "Дата рождения"
-            create_page.field_optional_filling(Locators.DATE_OF_BIRTH_FIELD_STUDENT_REG_FORM, 'Дата рождения:',
-                                                        BaseData.date_of_birth_student)
+        # Поле ввода "Предметы"
+        create_page.field_optional_filling(Locators.SUBJECTS_FIELD_STUDENT_REG_FORM, 'Предметы:',
+                                                    BaseData.subject_student)
 
-            # Поле ввода "Предметы"
-            create_page.field_optional_filling(Locators.SUBJECTS_FIELD_STUDENT_REG_FORM, 'Предметы:',
-                                                        BaseData.subject_student)
+        # Клик по чекбоксу "Хобби"
+        create_page.checkbox_click_without_text(Locators.RANDOM_CHECKBOX_STUDENT_REG_FORM)
 
-            # Клик по чекбоксу "Хобби"
-            create_page.checkbox_click_without_text(Locators.RANDOM_CHECKBOX_STUDENT_REG_FORM)
+        # Добавление изображения
+        create_page.add_photo(Locators.ADD_PICTURE_STUDENT_REG_FORM, BaseData.link_add_picture_student)
 
-            # Добавление изображения
-            create_page.add_photo(Locators.ADD_PICTURE_STUDENT_REG_FORM, BaseData.link_add_picture_student)
+        # Прокрутка страницы вниз
+        create_page.scrolling_page_down()
 
-            # Прокрутка страницы вниз
-            create_page.scrolling_page_down()
+        # Поле ввода "Текущий адрес"
+        create_page.field_optional_filling(Locators.CURRENT_ADDRESS_FIELD_STUDENT_REG_FORM, 'Текущий адрес:',
+                                                    BaseData.current_address_student)
 
-            # Поле ввода "Текущий адрес"
-            create_page.field_optional_filling(Locators.CURRENT_ADDRESS_FIELD_STUDENT_REG_FORM, 'Текущий адрес:',
-                                                        BaseData.current_address_student)
+        # Выбор штата
+        create_page.list_dropdown_down_enter_choose(Locators.STATE_LIST_STUDENT_REG_FORM,
+                                                             Locators.STATE_LIST_HARYANA_NAME_STUDENT_REG_FORM)
 
-            # Выбор штата
-            create_page.list_dropdown_down_enter_choose(Locators.STATE_LIST_STUDENT_REG_FORM,
-                                                                 Locators.STATE_LIST_HARYANA_NAME_STUDENT_REG_FORM)
+        # Выбор города
+        create_page.list_dropdown_down_enter_choose(Locators.CITY_LIST_STUDENT_REG_FORM,
+                                                             Locators.CITY_LIST_KARNAL_NAME_STUDENT_REG_FORM)
 
-            # Выбор города
-            create_page.list_dropdown_down_enter_choose(Locators.CITY_LIST_STUDENT_REG_FORM,
-                                                                 Locators.CITY_LIST_KARNAL_NAME_STUDENT_REG_FORM)
+        # Проверка текста в футере
+        create_page.text_on_pages(Locators.FOOTER_TEXT,
+                                           '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.')
 
-            # Проверка текста в футере
-            create_page.text_on_pages(Locators.FOOTER_TEXT,
-                                               '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.')
-
-        finally:
-            Methods.get_screenshot(self)
-            self.driver.quit()
+        # Скриншот страницы
+        Methods.get_screenshot(self)
